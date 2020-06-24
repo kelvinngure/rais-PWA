afd397eeb539037404900c695f810a67fdaaf066
kelvinngure <njuguna.kelvin22@gmail.com>
2020-06-24 16:49:14 -0400

    c

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,37 @@
-import React from "react"
+import React, {useState} from "react"
 import "./App.css"
 
+const Updater = async(e) => {
+    // handle updating the dropdown options
+    // handle enter key
+    if (e.key === "Enter"){
+        console.log("enter")
+    }
+    else{  
+        const searchTerm = e.target.value
+        console.log(searchTerm)
+    }
+    // const url = `https://restcountries.eu/rest/v2/name/${searchTerm}`
+
+    // const response = await fetch(url)
+    // const jsoned = await response.json()
+    // console.log(jsoned)
+}
+
+
+const Sender = (e) => {
+
+}
+
+
 const Search = () => {
+
+    const [search, setSearch] = useState("search for a country...")
+
     return(
         <div className = "Search">
             <p> Explore </p>
-            <input placeholder = "search for a country..." className = "searchBox"/>
+            <input id = "countrySearch" value = {search} className = "searchBox" onKeyDown = {Updater}/>
         </div>
     )
 }
